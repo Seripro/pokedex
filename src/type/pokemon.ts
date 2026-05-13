28e2d6410dc4e4fdf2d8aef9be3cef4568a0fe6e
keigo <2364138keigo@gmail.com>
2026-05-13 10:45:33 +0900

    単一取得、複数取得の場合のデータの型を定義

---
+++ src/type/pokemon.ts
@@ -0,0 +1,12 @@
+export type Pokemon = {
+  id: string;
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+};
+
+export type PokemonNameAndUrl = {
+  name: string;
+  url: string;
+};
