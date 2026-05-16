743e159de1cb364972f0ea6ea1ed43369a26ba1d
keigo <2364138keigo@gmail.com>
2026-05-16 18:21:55 +0900

    取得に使う関数を変更（詳細を取得する）

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,20 @@
 import { useEffect, useState } from "react";
 import { PokemonCard } from "../components/PokemonCard";
-import type { Pokemon, PokemonNameAndUrl } from "../type/pokemon";
-import { fetchPokemonList, getIdFromUrl } from "../api/pokemon";
+import type { PokemonDetailType } from "../type/pokemon";
+import { getPokemonsDetail } from "../api/pokemon";
 import { Link } from "react-router-dom";
 import { Modal } from "../components/Modal";
 
 export const Home = () => {
-  const [data, setData] = useState<Pokemon[]>();
+  const [data, setData] = useState<PokemonDetailType[]>();
   const [isModalOpen, setModalOpen] = useState(false);
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
   useEffect(() => {
     const fetchData = async () => {
-      const allData: PokemonNameAndUrl[] = await fetchPokemonList("?limit=100");
-      const data = allData.map((poke) => {
-        const id = getIdFromUrl(poke.url);
-        return {
-          id: id,
-          name: poke.name,
-          sprites: {
-            front_default: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-          },
-        };
-      });
-      setData(data);
+      const allData = await getPokemonsDetail(1, 151);
+      setData(allData);
     };
     fetchData();
   }, []);
